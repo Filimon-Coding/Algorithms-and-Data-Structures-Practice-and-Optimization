82f5c338b9c6776eead5506054f46a762b488beb
Filimon_Coding <143404004+Filimon-Coding@users.noreply.github.com>
2024-08-29 19:36:27 +0200

    disk

---
+++ src/DiskTest01.java
@@ -0,0 +1,13 @@
+public class DiskTest01 {
+    public static void main(String[] args) {
+        int n = 11;
+        boolean svar = p(n);
+        System.out.println(n + " > 10 = " + svar);
+
+    }
+    public static boolean p(int x){
+        return x >10;
+    }
+
+
+}
