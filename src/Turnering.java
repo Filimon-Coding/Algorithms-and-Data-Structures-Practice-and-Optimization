e6c079ebacdf4e26b4982d5fc9f24de4ce77a2a6
Filimon_Coding <143404004+Filimon-Coding@users.noreply.github.com>
2024-10-14 06:39:26 +0200

    Update Turnering.java

--- src/Turnering.java
+++ src/Turnering.java
@@ -34,9 +34,74 @@ public class Turnering {
         return new int[]{største, nestStørste};  // Returner største og nest største
     }
 
+    public static int inversjoner(int[] a) {
+        int antall = 0;  // Teller antall inversjoner
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[i] > a[j]) {
+                    antall++;  // Øk antall inversjoner hvis elementene er i utakt
+                }
+            }
+        }
+        return antall;
+    }
+
+    public static void boblesorteringOptimalisert(int[] a) {
+        boolean byttet;  // Variabel som holder styr på om vi har byttet i en runde
+        for (int n = a.length; n > 1; n--) {
+            byttet = false;  // Sett byttet til false ved starten av hver runde
+            for (int i = 1; i < n; i++) {
+                if (a[i - 1] > a[i]) {
+                    bytt(a, i - 1, i);  // Bytt hvis elementene er i feil rekkefølge
+                    byttet = true;  // Marker at vi har gjort et bytte
+                }
+            }
+            // Hvis vi ikke har byttet i løpet av runden, er listen sortert
+            if (!byttet) {
+                break;
+            }
+        }
+    }
+
+    private static void bytt(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+
+
     public static void main(String[] args) {
         int[] tall = {3, 8, 1, 7, 6, 10, 4};
         int[] result = finnStørsteOgNestStørste(tall);
         System.out.println("Største: " + result[0] + ", Nest største: " + result[1]);
+
+        int[] a = {1, 2, 4, 3, 6, 7, 9, 5, 8, 10};
+        System.out.println(inversjoner(a));  // Utskrift: 5
+
+        System.out.println("________________________________________________" + "\n" +
+                "___________________________________________________________");
+
+        int[] tallA = {5, 1, 4, 2, 8};
+        int[] tallB = {1, 2, 3, 4, 5};
+
+        boblesorteringOptimalisert(tallA);
+        boblesorteringOptimalisert(tallB);
+
+        // Skriv ut det sorterte arrayet
+        for (int i : tallA) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println("\n" + "________________________________________________" + "\n" +
+                "_______________________________________________");
+
+        // Skriv ut det sorterte arrayet
+        for (int i : tallB) {
+            System.out.print(i + " ");
+        }
+
+
+
     }
 }
