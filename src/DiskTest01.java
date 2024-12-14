fde607a5908b391228b527f01e7bbdd803d44738
Filimon_Coding <143404004+Filimon-Coding@users.noreply.github.com>
2024-12-14 18:25:15 +0100

    Update DiskTest01.java

--- src/DiskTest01.java
+++ src/DiskTest01.java
@@ -1,6 +1,8 @@
 public class DiskTest01 {
     public static void main(String[] args) {
         int n = 11;
+int a = 11;
+int b = 11;
         boolean svar = p(n);
         System.out.println(n + " > 10 = " + svar);
 
